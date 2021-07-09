907f4840f7aa4bdf824f34ee3b1052983a68c534
Mike Penhallegon <mpenhallegon@domaintools.com>
2021-07-09 15:38:54 -0700

    changed requirements file

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="dc-demo-fastapi", # Replace with your own username
+    name="house-calandar", # Replace with your own username
     version="0.0.1",
     author="Micahel Penhallegon",
     author_email="mike@hematite.tech",
-    description="A small example package",
+    description="A simple api to handle adding, retrieving and managing house music events",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/mpenhall-celgene/dc-fast-example",
@@ -20,5 +20,9 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     packages=setuptools.find_packages(),
+    install_requires=[
+        "fastapi",
+        "uvicorn"
+    ],
     python_requires='>=3.6',
 )
