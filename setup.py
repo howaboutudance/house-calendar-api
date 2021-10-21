e0654d838079d66ea45eb5c4e5a896b95ab0d2cc
Mike Penhallegon <mpenhallegon@domaintools.com>
2021-10-21 10:28:07 -0700

    updated version and added updated egg metadata

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="house_calendar_api", # Replace with your own username
-    version="0.5.1",
+    version="0.5.3",
     author="Micahel Penhallegon",
     author_email="mike@hematite.tech",
     description="A simple api to handle adding, retrieving and managing house music events",
@@ -23,6 +23,9 @@ setuptools.setup(
     install_requires=[
         "fastapi",
         "uvicorn",
+        "asyncpg",
+        "alembic",
+        "sqlmodel"
     ],
     python_requires='>=3.6',
 )
