b0314de61936fd994ec7487220769e1f89834eb5
Mike Penhallegon <mpenhallegon@domaintools.com>
2021-10-21 11:16:29 -0700

    fixed for mypy checks with empty __init__ on base

--- house_calendar/db/tables_models.py
+++ house_calendar/db/tables_models.py
@@ -1,8 +1,7 @@
 from typing import Text
-from sqlalchemy import Index, ForeignKey, Column
+from sqlalchemy import Column
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.ext.declarative import as_declarative, declared_attr
-from sqlalchemy.sql.schema import Sequence
+from sqlalchemy.orm import as_declarative, declared_attr
 import uuid
 
 from sqlalchemy.sql.sqltypes import JSON, DateTime, String
@@ -11,12 +10,16 @@ from sqlalchemy.sql.sqltypes import JSON, DateTime, String
 class Base():
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     __name__: str
+
+    def __init__(self, *args, **kwargs) -> None:
+        ...
+
     @declared_attr
     def __tablename__(cls) -> Text:
         return cls.__name__.lower()
 
 class EventTable(Base):
-    __tablename__ = "event"
+    __name__ = "event"
     name = Column(String)
     start_date = Column(DateTime)
     end_date = Column(DateTime)
