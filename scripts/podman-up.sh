230949da4077fbed8f01832865fa55c9d2548845
Michael Penhallegon <mike@hematite.tech>
2021-12-04 20:04:46 -0800

    Continues fix by including network on roll-out down script

--- scripts/podman-up.sh
+++ scripts/podman-up.sh
@@ -3,7 +3,7 @@ set +e
 
 #Checks if network exists
 #TODO: #7 parameterize network name
-if [ "$(podamn network exists house-calendar-backend)" == "1"] ; then
+if [ "$(podman network exists house-calendar-backend)" == "1" ] ; then
   podman network create house-calendar-backend
 fi
 
@@ -11,15 +11,16 @@ fi
 #TODO: Investigate: pod setup with shell script
 if [ "$(podman pod ps |grep -E "api_pod|phppgadmin_pod|db" | wc -l)" == "0" ] ; then
   echo "> > > Starting PostgreSQL, house_calendar and phppgadmin"
-  podman play kube scripts/pods/backend.yaml
+  podman play kube --network=house-calendar-backend scripts/pods/backend.yaml
 else
   echo "Development pod is already running. Re-create it? Y/N"
   read input
+  # TODO: Fix to hand yes and y cass
   if [ $input == "Y" ] ; then
     podman pod rm api_pod -f
     podman pod rm phppgadmin_pod -f
     podman pod rm db -f
-    podman play kube scripts/pods/backend.yaml
+    podman play kube --network=house-calendar-backend scripts/pods/backend.yaml
   else
     echo "Leaving bootstrap process."
     exit 0
